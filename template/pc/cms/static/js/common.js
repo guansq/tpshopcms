13d3035e80c223a847dccd0fba7b5e197b7c9f76
hubo <hubo@ruitukeji.com>
2018-02-07 16:33:03 +0800

    'f'

--- template/pc/cms/static/js/common.js
+++ template/pc/cms/static/js/common.js
@@ -8,14 +8,14 @@ $(function () {
     });
   });
   
-  $(".has-sub").mouseout(function () {
-    $(this).find(".sub-nav").css({
-      "height": "0",
-      "opacity": "0",
-      "overflow": "hidden"
-    });
-  })
-  
+  // $(".has-sub").mouseout(function () {
+  //   $(this).find(".sub-nav").css({
+  //     "height": "0",
+  //     "opacity": "0",
+  //     "overflow": "hidden"
+  //   });
+  // })
+  //
   $(".has-subs").mouseover(function () {
     $(this).find(".sub-navs").css({
       "display": "block",
@@ -25,13 +25,13 @@ $(function () {
     });
   });
   
-  $(".has-subs").mouseout(function () {
-    $(this).find(".sub-navs").css({
-      "height": "0",
-      "opacity": "0",
-      "overflow": "hidden"
-    });
-  })
+  // $(".has-subs").mouseout(function () {
+  //   $(this).find(".sub-navs").css({
+  //     "height": "0",
+  //     "opacity": "0",
+  //     "overflow": "hidden"
+  //   });
+  // })
   
   // go top
   $('#topBtn').click(function (event) {
