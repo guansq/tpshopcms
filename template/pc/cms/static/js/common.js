d741e0073c76e6f39b739e6e479053374a8371a7
hubo <hubo@ruitukeji.com>
2018-02-08 10:30:36 +0800

    'f'

--- template/pc/cms/static/js/common.js
+++ template/pc/cms/static/js/common.js
@@ -61,10 +61,8 @@ var winH = $(window).height();
 window.onscroll = function () {
   var scrollTop = $(document).scrollTop();
   var DH = $(document.body).height();
-  var scrollH =  $(document.body).scrollHeight;
-  // console.log(document);
   // console.log(scrollTop);
-  console.log(scrollH);
+  // console.log(winH);
   if(scrollTop > 300){
      $('.topBtn').addClass('show')
   }else{
